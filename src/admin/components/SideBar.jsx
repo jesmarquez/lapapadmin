4eefc85b052f044915350cdf616f89526b793cf2
DESKTOP-5G35IDM\Jesus Marquez <jesmqz@gmail.com>
2023-07-23 01:48:40 -0400

    Add and change side bar

--- src/admin/components/SideBar.jsx
+++ src/admin/components/SideBar.jsx
@@ -2,6 +2,9 @@ import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import PersonIcon from '@mui/icons-material/Person';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import RoomServiceIcon from '@mui/icons-material/RoomService';
 
 export const SideBar = ({ drawerWidth = 240 }) => {
   return (
@@ -20,7 +23,7 @@ export const SideBar = ({ drawerWidth = 240 }) => {
       >
         <Toolbar/>
         <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
@@ -29,16 +32,41 @@ export const SideBar = ({ drawerWidth = 240 }) => {
               <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
-        ))}
-
+        ))} */}
+          <ListItem key={ 'AssignmentIndIcon' } disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <AssignmentIndIcon/>
+              </ListItemIcon>
+              <ListItemText primary={'Users'} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem key={ 'Food' } disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <FastfoodIcon/>
+              </ListItemIcon>
+              <ListItemText primary={'Foods'} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem key={ 'RoomServiceIcon' } disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <RoomServiceIcon/>
+              </ListItemIcon>
+              <ListItemText primary={'Orders'} />
+            </ListItemButton>
+          </ListItem>
           <ListItem key={ 'Customer' } disablePadding>
             <ListItemButton>
               <ListItemIcon>
                 <PersonIcon/>
               </ListItemIcon>
-              <ListItemText primary={'Customer'} />
+              <ListItemText primary={'Customers'} />
             </ListItemButton>
           </ListItem>
+
+
       </List>
 
       </Drawer>
